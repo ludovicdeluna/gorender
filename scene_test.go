9d80a807398d8f909d8814ae39ecfbf0c822dc1f
Ludovic de Luna <ludovic@deluna.fr>
2016-09-10 01:08:29 +0200

    simplify iterator

--- scene_test.go
+++ scene_test.go
@@ -27,16 +27,16 @@ func TestScene_NewScene(t *testing.T) {
 		}
 		for _, testSize := range testCases {
 			t.Run(testSize.title, func(t *testing.T) {
-				if e := expect(testSize.got, testSize.want); e.Equals() {
-					t.Error(e.Is("New scene get correct sizes"))
+				if e := expect(testSize.got); e.Equals(testSize.want) {
+					t.Error(e.It("New scene get correct sizes"))
 				}
 			})
 		}
 	}
 	t.Run("Points", func(t *testing.T) {
 		rect := image.Rect(0, 0, size.width, size.height)
-		if e := expect(scene.Image.Bounds(), rect); e.Equals() {
-			t.Error(e.Is("scene.Image as correct rectangle points"))
+		if e := expect(scene.Image.Bounds()); e.Equals(rect) {
+			t.Error(e.It("scene.Image as correct rectangle points"))
 		}
 	})
 }
@@ -51,16 +51,14 @@ func TestScene_EachPixel(t *testing.T) {
 		done := make(chan Point)
 		go func() {
 			point := Point{0, 0, "0-0"}
-			var current_point Point
 			for point.y < max {
 				point.title = fmt.Sprintf("%d-%d", point.x, point.y)
-				current_point = point
+				done <- point
 				point.x = point.x + 1
 				if point.x >= max {
 					point.x = 0
 					point.y = point.y + 1
 				}
-				done <- current_point
 			}
 			close(done)
 		}()
@@ -73,8 +71,8 @@ func TestScene_EachPixel(t *testing.T) {
 	})
 	for point := range pixelIterator(scene.Width) {
 		t.Run("pixel_"+point.title, func(t *testing.T) {
-			if e := expect(scene.Image.At(point.x, point.y), testCase); e.Equals() {
-				t.Error(e.Is("Color for this pixel is random"))
+			if e := expect(scene.Image.At(point.x, point.y)); e.Equals(testCase) {
+				t.Error(e.It("Color for this pixel is random"))
 			}
 		})
 	}
@@ -90,15 +88,15 @@ func TestScene_Save(t *testing.T) {
 	testName = "mustFail"
 	t.Run(testName, func(t *testing.T) {
 		testCase := testCases[testName]
-		if e := expect(scene.Save(testCase), "Can't save file " + testCase); e.HasError() {
-			t.Error(e.Is("Save on no-writable location will fail"))
+		if e := expect(scene.Save(testCase)); e.HasError("Can't save file " + testCase) {
+			t.Error(e.It("Save on no-writable location will fail"))
 		}
 	})
 	testName = "mustSucceed"
 	t.Run(testName, func(t *testing.T) {
 		testCase := testCases[testName]
-		if e := expect(scene.Save(testCase), error(nil)); e.Equals() {
-			t.Error(e.Is("Save must be succeed"))
+		if e := expect(scene.Save(testCase)); e.Equals(error(nil)) {
+			t.Error(e.It("Save must be succeed"))
 		}
 	})
 }
@@ -112,37 +110,38 @@ func randomColor() color.RGBA {
 	return color.RGBA{rgb[0], rgb[1], rgb[2], byte(255)}
 }
 
-
 // Expects helper (to avoid repetition -> if got, want := , ; got != want)
 type Expects struct {
-	got		interface{}
-	want	interface{}
+	got   interface{}
+	want  interface{}
 	title string
 }
 
-func expect(got, want interface{}) *Expects {
-	expects := Expects{got: got, want: want}
+func expect(got interface{}) *Expects {
+	expects := Expects{got: got}
 	return &expects
 }
 
-func(e *Expects) String() string {
+func (e *Expects) String() string {
 	return fmt.Sprintf(msg, e.title, e.got, e.want)
 }
 
-func(e *Expects) Is(m string) string {
+func (e *Expects) It(m string) string {
 	e.title = m
 	return e.String()
 }
 
-func(e *Expects) Equals() bool {
+func (e *Expects) Equals(want interface{}) bool {
+	e.want = want
 	return e.got != e.want
 }
 
-func(e *Expects) HasError() bool {
+func (e *Expects) HasError(want interface{}) bool {
+	e.want = want
 	switch t := e.got.(type) {
 	case error:
-		return t != nil && t.Error() != e.want
+		return t.Error() != e.want
 	default:
-		return false
+		return true
 	}
 }
